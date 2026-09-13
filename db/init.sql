9760db6d285eb3cf8c05faeb76122ee93b680184
Tuan <vuanhtuanvn85@gmail.com>
2026-09-13 22:05:40 +0700

    Revert "test"
    This reverts commit 3a2753ad3fd40a22bf5935b87bcdc4c24311452c.

--- db/init.sql
+++ db/init.sql
@@ -17,5 +17,6 @@ INSERT INTO words (word, definition) VALUES
   ('flower',   'bông hoa'),
   ('guitar',   'đàn guitar'),
   ('house',    'ngôi nhà'),
+  ('internet', 'mạng internet'),
   ('juice',    'nước ép')
 ON CONFLICT (word) DO NOTHING;
